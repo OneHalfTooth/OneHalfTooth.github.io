64fe29f491179d4d04c153dd66c5b41e15a62c10
马少洋 <mayang_email@sina.cn>
2016-09-26 11:08:56 +0800

    Site updated: 2016-09-26 11:08:56

--- html/PersonalProfile/js/personalProfile.js
+++ html/PersonalProfile/js/personalProfile.js
@@ -1,2 +1,8 @@
 // JavaScript Document
 
+
+
+function push_app_store(i) {
+    var url_arr = ["https://itunes.apple.com/cn/app/jiu-yun-da-mai-jiu-jiu-shang/id960210865?mt=8","https://itunes.apple.com/cn/app/xiao-da-kuai-pao/id1079848152?mt=8","https://itunes.apple.com/cn/app/xiao-niu-ji-pei/id1116898640?mt=8","https://itunes.apple.com/cn/app/xiang-le-me/id1067738843?mt=8","https://itunes.apple.com/cn/app/mai-mai-tong-xiang-le-me/id1091138907?mt=8"]
+    window.open(url_arr[i])
+}
