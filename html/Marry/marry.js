a6a9b1bcead617a6f2e00a9ddbf74d6a6472adc6
马少洋 <mayang_email@sina.cn>
2016-12-19 13:43:51 +0800

    Site updated: 2016-12-19 13:43:50

---
+++ html/Marry/marry.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mayang on 2016/12/16.
+ */
+
+var music_start = 'https://coding.net/u/ma_yang/p/source/git/raw/master/Marry/stopmusic_logo.png'
+
+$(document).ready(function () {
+
+    // $('.play_music').hide();
+    var audio = document.createElement('audio');
+    audio.src = 'https://coding.net/u/ma_yang/p/source/git/raw/master/Marry/need_love_you.mp3'//这里放音乐的地址
+    audio.autoplay = true
+    audio.id = 'music'
+    document.body.appendChild(audio)
+
+
+    $('.paly_music').click(function () {
+        console.log('播放')
+        $(this).hide()
+        $('.stop_music').show()
+        audio.play();
+    });
+    $('.stop_music').click(function () {
+        console.log('停止')
+        $(this).hide()
+        $('.paly_music').show()
+        audio.pause()
+    })
+})
